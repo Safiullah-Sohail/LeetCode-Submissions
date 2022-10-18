f2a958b2b6ef09e84a3a9632b616d02345de481d
Safiullah-Sohail <106798707+Safiullah-Sohail@users.noreply.github.com>
2022-10-18 16:57:01 +0500

    Time: 9 ms (5.18%), Space: 7.6 MB (77.67%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.cpp
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -1,21 +1,81 @@
-class Solution {
-public:
-    int lastStoneWeight(vector<int>& stones) {
-        priority_queue<int, vector<int>> q;
-        int x, y;
-        
-        for(auto x:stones)
-            q.push(x);
-        
-        while(q.size() != 1){
-            y = q.top();
-            q.pop();
-            x = q.top();
-            q.pop();
+class priorityQueue
+{
+    vector<int> heapArr;
+    int size;
 
-            q.push(y-x);
+public:
+    priorityQueue()
+    {
+        size = 0;
+    }
+    int parentIndex(int i)
+    {
+        return (i - 1);
+    }
+    void swap(int a, int b)
+    {
+        int temp = heapArr[a];
+        heapArr[a] = heapArr[b];
+        heapArr[b] = temp;
+    }
+    void siftUp(int index)
+    {
+        while (index > 0)
+        {
+            int pIdx = parentIndex(index);
+            if (heapArr[index] > heapArr[pIdx])
+            {
+                swap(index, pIdx);
+                index = pIdx;
+            }
+            else
+            {
+                break;
+            }
         }
-        
-        return q.top();
+    }
+    void insert(int value)
+    {
+        heapArr.push_back(value);
+        int index = size;
+        size++;
+        siftUp(index);
+    }
+    vector<int> getQueue()
+    {
+        return heapArr;
+    }
+    int length()
+    {
+        return heapArr.size();
+    }
+    int pop()
+    {
+        if(heapArr.size()>0)
+        {
+            int temp = heapArr[0];
+            heapArr.erase(heapArr.begin());
+            size--;
+            return temp;
+        }
+        return -1;
+    }
+};
+class Solution
+{
+public:
+    int lastStoneWeight(vector<int> &stones)
+    {
+        priorityQueue q;
+        int num1, num2;
+        for (int n=0 ; n<stones.size() ; n++)
+            q.insert(stones[n]);
+        while (q.length() != 1)
+        {
+            num2 = q.pop();
+            num1 = q.pop();
+            q.insert(num2 - num1);
+        }
+        return q.pop();
     }
 };
