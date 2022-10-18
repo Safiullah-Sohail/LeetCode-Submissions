379038c6d829d29e62e64b010c2c2002c8728053
Safiullah-Sohail <106798707+Safiullah-Sohail@users.noreply.github.com>
2022-10-18 15:49:32 +0500

    Time: 17 ms (83.70%), Space: 10.4 MB (53.53%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.cpp
+++ 0506-relative-ranks/0506-relative-ranks.cpp
@@ -1,85 +1,35 @@
-class priorityQueue
-{
-    vector<pair<int,int>> heapArr;
-    int size;
-public:
-    priorityQueue()
-    {
-        size = 0;
-    }
-    int parentIndex(int i)
-    {
-        return (i - 1);
-    }
-    int leftChildIndex(int i)
-    {
-        return (2 * i) + 1;
-    }
-    int rightChildIndex(int i)
-    {
-        return (2 * i) + 2;
-    }
-    void swap(int a, int b)
-    {
-        pair<int,int> temp  = heapArr[a];
-        heapArr[a] = heapArr[b];
-        heapArr[b] = temp;
-    }
-    void siftUp(int index)
-    {
-        while (index > 0)
-        {
-            int pIdx = parentIndex(index);
-            if (heapArr[index].first > heapArr[pIdx].first)
-            {
-                swap(index, pIdx);
-                index = pIdx;
-            }
-            else
-            {
-                break;
-            }
-        }
-    }
-
-    void insert(pair<int,int> value)
-    {
-        heapArr.push_back(value);
-        int index = size;
-        size++;
-        siftUp(index);
-    }
-
-    vector<pair<int,int>> getQueue()
-    {
-        return heapArr;
-    }
-};
 class Solution
 {
-    priorityQueue q = priorityQueue();
 public:
     vector<string> findRelativeRanks(vector<int> &score)
     {
-        for (int n = 0; n < score.size(); n++)
+        vector<pair<int, int>> priQue;
+        for (int i = 0; i < score.size(); i++)
         {
-            pair<int,int> t(score[n],n);
-            q.insert(t);
+            priQue.push_back({score[i], i});
         }
-        vector<pair<int,int>> temp = q.getQueue();
-        string medal = "";
-        vector<string> answer(temp.size());
-        for(int n = 0 ; n < temp.size() ; n++)
+        sort(priQue.rbegin(), priQue.rend());
+        vector<string> answer(score.size());
+        int n = score.size();
+        for (int i = 0; i < n; i++)
         {
-            if(n == 0)
-                medal = "Gold Medal";
-            else if(n == 1)
-                medal = "Silver Medal";
-            else if(n == 2)
-                medal = "Bronze Medal";
+            if (i == 0)
+            {
+                answer[priQue[i].second] = "Gold Medal";
+            }
+            else if (i == 1)
+            {
+                answer[priQue[i].second] = "Silver Medal";
+            }
+
+            else if (i == 2)
+            {
+                answer[priQue[i].second] = "Bronze Medal";
+            }
             else
-                medal = to_string(n + 1);
-            answer[temp[n].second] = medal;
+            {
+                answer[priQue[i].second] = to_string(i + 1);
+            }
         }
         return answer;
     }
