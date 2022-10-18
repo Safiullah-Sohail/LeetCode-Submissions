aead2c3ba62a9a2fcbc5bcb4d697ca759335e101
Safiullah-Sohail <106798707+Safiullah-Sohail@users.noreply.github.com>
2022-10-18 15:44:37 +0500

    Time: 492 ms (5.01%), Space: 10.4 MB (52.64%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.cpp
+++ 0506-relative-ranks/0506-relative-ranks.cpp
@@ -1,18 +1,86 @@
-class Solution {
+class priorityQueue
+{
+    vector<pair<int,int>> heapArr;
+    int size;
 public:
-    vector<string> findRelativeRanks(vector<int>& score) 
+    priorityQueue()
     {
-        priority_queue<pair<int,int>>q;
-        for(int i=0;i<size(score);i++)
-            q.push({score[i],i});
-        int i=0;
-        vector<string>a(size(q));
-        while(!q.empty())
+        size = 0;
+    }
+    int parentIndex(int i)
+    {
+        return (i - 1);
+    }
+    int leftChildIndex(int i)
+    {
+        return (2 * i) + 1;
+    }
+    int rightChildIndex(int i)
+    {
+        return (2 * i) + 2;
+    }
+    void swap(int a, int b)
+    {
+        pair<int,int> temp  = heapArr[a];
+        heapArr[a] = heapArr[b];
+        heapArr[b] = temp;
+    }
+    void siftUp(int index)
+    {
+        while (index > 0)
         {
-            a[q.top().second]=i==0?"Gold Medal":i==1?"Silver Medal":i==2?"Bronze Medal":to_string(i+1);
-            i++;
-            q.pop();
+            int pIdx = parentIndex(index);
+            if (heapArr[index].first > heapArr[pIdx].first)
+            {
+                swap(index, pIdx);
+                index = pIdx;
+            }
+            else
+            {
+                break;
+            }
         }
-        return a;
+    }
+
+    void insert(pair<int,int> value)
+    {
+        heapArr.push_back(value);
+        int index = size;
+        size++;
+        siftUp(index);
+    }
+
+    vector<pair<int,int>> getQueue()
+    {
+        return heapArr;
+    }
+};
+class Solution
+{
+    priorityQueue q = priorityQueue();
+public:
+    vector<string> findRelativeRanks(vector<int> &score)
+    {
+        for (int n = 0; n < score.size(); n++)
+        {
+            pair<int,int> t(score[n],n);
+            q.insert(t);
+        }
+        vector<pair<int,int>> temp = q.getQueue();
+        string medal = "";
+        vector<string> answer(temp.size());
+        for(int n = 0 ; n < temp.size() ; n++)
+        {
+            if(n == 0)
+                medal = "Gold Medal";
+            else if(n == 1)
+                medal = "Silver Medal";
+            else if(n == 2)
+                medal = "Bronze Medal";
+            else
+                medal = to_string(n + 1);
+            answer[temp[n].second] = medal;
+        }
+        return answer;
     }
 };
