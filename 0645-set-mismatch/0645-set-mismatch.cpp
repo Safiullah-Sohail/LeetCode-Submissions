b3702e5e98f0a7554c63ab7a3028e76d314ea947
Safiullah-Sohail <106798707+Safiullah-Sohail@users.noreply.github.com>
2022-10-24 15:02:39 +0500

    Time: 46 ms (81.75%), Space: 21.2 MB (99.95%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -1,28 +1,28 @@
-class Solution {
+class Solution
+{
 public:
-    vector<int> findErrorNums(vector<int>& nums) {
-        vector<int> ans;
+    vector<int> findErrorNums(vector<int> &nums)
+    {
+        vector<int> errz;
         int n = 0;
-        while(n<nums.size())
+        while (n < nums.size())
         {
-            int correct = nums[n]-1;
-            if( nums[n]!=nums[correct])
+            int temp = nums[n] - 1;
+            if (nums[n] != nums[temp])
             {
-                swap(nums[n],nums[correct]);
+                swap(nums[n], nums[temp]);
             }
             else
-            {
                 n++;
-            }
         }
-        for(int i=0;i<nums.size();i++)
+        for (int i = 0; i < nums.size(); i++)
         {
-            if(nums[i]!=i+1 )
+            if (nums[i] != i + 1)
             {
-                ans.push_back(nums[i]);
-                ans.push_back(i+1);
+                errz.push_back(nums[i]);
+                errz.push_back(i + 1);
             }
         }
-        return ans;
+        return errz;
     }
 };
