69913adc18509c2e5d257dc14366d2a4468b5e0b
Safiullah-Sohail <106798707+Safiullah-Sohail@users.noreply.github.com>
2022-10-24 13:51:20 +0500

    Time: 650 ms (32.06%), Space: 116.1 MB (50.94%) - LeetHub

---
+++ 0575-distribute-candies/0575-distribute-candies.cpp
@@ -0,0 +1,25 @@
+class Solution
+{
+public:
+    int distributeCandies(vector<int> &candyType)
+    {
+        unordered_map<int, int> m;
+        for (int i=0 ; i<candyType.size() ; i++)
+            m[candyType[i]]++;
+        int allowed = candyType.size() / 2;
+        if (allowed >= m.size())
+            return m.size();
+        auto it = m.begin();
+        int eaten = 0;
+        while (it != m.end())
+        {
+            if (it->second > 0)
+                it->second--;
+            eaten++;
+            if (eaten > allowed)
+                break;
+            it++;
+        }
+        return eaten - 1;
+    }
+};
