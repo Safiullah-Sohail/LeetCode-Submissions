08bc864875a043ee37096e16110f691683eaba2c
Safiullah-Sohail <106798707+Safiullah-Sohail@users.noreply.github.com>
2022-10-24 15:35:56 +0500

    Time: 0 ms (100.00%), Space: 6.6 MB (53.13%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,54 +1,59 @@
-class Solution {
+class Solution
+{
 public:
-  vector<string> letterCombinations(string d){
-      unordered_map<char, string> mp;
-      mp['2'] = "abc";
-      mp['3'] = "def";
-      mp['4'] = "ghi";
-      mp['5'] = "jkl";
-      mp['6'] = "mno";
-      mp['7'] = "pqrs";
-      mp['8'] = "tuv";
-      mp['9'] = "wxyz";
-        
-      int n = d.size(),i = 0; vector<string> res;
-      string one = mp[d[i]];
-      for (int j = 0; j < one.size(); j++){
-          string temp1 = "";
-          temp1.push_back(one[j]);
-          if (i + 1 < n){
-          
-              string two = mp[d[i + 1]];
-          
-              for (int k = 0; k < two.size(); k++){
-            
-                  string temp2 = temp1;
-            
-                  temp2.push_back(two[k]);
-                  if (i + 2 < n){
-              
-                      string three = mp[d[i + 2]];
-
-                      for (int l = 0; l < three.size(); l++){
-
-                          string temp3 = temp2;
-
-                          temp3.push_back(three[l]);
-                          if (i + 3 < n){
-
-                              string four = mp[d[i + 3]];
-
-                              for (int m = 0; m < four.size(); m++){
-                                  string temp4 = temp3;
-                                  temp4.push_back(four[m]);
-                                  res.push_back(temp4);
-                              }
-                          }else res.push_back(temp3);
-                      }
-                  }else res.push_back(temp2);
-              }
-          }else res.push_back(temp1);
-      }
-      return res;
+    vector<string> letterCombinations(string digits)
+    {
+        unordered_map<char, string> keyMap;
+        keyMap['2'] = "abc";
+        keyMap['3'] = "def";
+        keyMap['4'] = "ghi";
+        keyMap['5'] = "jkl";
+        keyMap['6'] = "mno";
+        keyMap['7'] = "pqrs";
+        keyMap['8'] = "tuv";
+        keyMap['9'] = "wxyz";
+        int size = digits.size(), i = 0;
+        vector<string> ans;
+        string one = keyMap[digits[i]];
+        for (int j = 0; j < one.size(); j++)
+        {
+            string temp = "";
+            temp.push_back(one[j]);
+            if (i + 1 < size)
+            {
+                string two = keyMap[digits[i + 1]];
+                for (int k = 0; k < two.size(); k++)
+                {
+                    string temp2 = temp;
+                    temp2.push_back(two[k]);
+                    if (i + 2 < size)
+                    {
+                        string three = keyMap[digits[i + 2]];
+                        for (int l = 0; l < three.size(); l++)
+                        {
+                            string temp3 = temp2;
+                            temp3.push_back(three[l]);
+                            if (i + 3 < size)
+                            {
+                                string four = keyMap[digits[i + 3]];
+                                for (int m = 0; m < four.size(); m++)
+                                {
+                                    string temp4 = temp3;
+                                    temp4.push_back(four[m]);
+                                    ans.push_back(temp4);
+                                }
+                            }
+                            else
+                                ans.push_back(temp3);
+                        }
+                    }
+                    else
+                        ans.push_back(temp2);
+                }
+            }
+            else
+                ans.push_back(temp);
+        }
+        return ans;
     }
 };
