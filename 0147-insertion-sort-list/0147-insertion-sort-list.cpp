56bf412ad6f4b7641270f5b195f867b1975016f6
Safiullah-Sohail <106798707+Safiullah-Sohail@users.noreply.github.com>
2022-11-07 15:08:17 +0500

    Time: 135 ms (10.75%), Space: 9.7 MB (18.30%) - LeetHub

--- 0147-insertion-sort-list/0147-insertion-sort-list.cpp
+++ 0147-insertion-sort-list/0147-insertion-sort-list.cpp
@@ -8,22 +8,24 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
+class Solution
+{
 public:
-ListNode* insertionSortList(ListNode* head) {
-
-    if(head == nullptr) return nullptr;
-    
-    int temp = 0;
-    for(ListNode* curr = head; curr != nullptr; curr = curr->next){
-        for(ListNode* prev = head; prev != curr; prev = prev->next){
-            if(curr->val < prev->val){
-                temp = curr->val;
-                curr->val = prev->val;
-                prev->val = temp;
+    ListNode *insertionSortList(ListNode *head)
+    {
+        if (head == nullptr)
+            return nullptr;
+        int temp = 0;
+        for (ListNode * current = head;  current != nullptr;  current = current->next)
+        {
+            for (ListNode *prev = head; prev !=  current; prev = prev->next)
+            {
+                if ( current->val < prev->val)
+                {
+                    swap(current->val,prev->val);
+                }
             }
         }
+        return head;
     }
-    return head;
-}
 };
