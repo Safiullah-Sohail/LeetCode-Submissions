35199b4552012141917f77706d6d3136bf5b7585
Safiullah-Sohail <106798707+Safiullah-Sohail@users.noreply.github.com>
2022-11-19 11:59:34 +0500

    Time: 4 ms (66.27%), Space: 8.9 MB (9.47%) - LeetHub

---
+++ 0274-h-index/0274-h-index.cpp
@@ -0,0 +1,38 @@
+class Solution
+{
+    void countSort(vector<int> &array)
+    {
+        int size = array.size();
+        int max = *max_element(array.begin(),array.end());
+        vector<int> output(size);
+        vector<int> count(max + 1, 0);
+        for (int i = 0; i < size; i++)
+            count[array[i]]++;
+
+        for (int i = 1; i <= max; i++)
+            count[i] += count[i - 1];
+
+        for (int i = size - 1; i >= 0; i--)
+        {
+            output[count[array[i]] - 1] = array[i];
+            count[array[i]]--;
+        }
+        for (int i=0 ; i<size ; i++)
+            array[i] = output[size-1-i];
+    }
+
+public:
+    int hIndex(vector<int> &citations)
+    {
+        int index;
+        countSort(citations);
+        int n = citations.size();
+        int count = 0;
+        for (index = 0; index<n ; index++)
+        {
+            if(index >= citations[index])
+                break;
+        }
+        return index;
+    }
+};
