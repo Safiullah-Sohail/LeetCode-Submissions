02411c3d975a848f48be9c68b06e2a207a4009ac
Safiullah-Sohail <106798707+Safiullah-Sohail@users.noreply.github.com>
2022-11-19 12:06:18 +0500

    Time: 3 ms (78.23%), Space: 8.1 MB (70.55%) - LeetHub

--- 1051-height-checker/1051-height-checker.cpp
+++ 1051-height-checker/1051-height-checker.cpp
@@ -1,10 +1,29 @@
 class Solution
 {
+    vector<int> countSort(vector<int> array)
+    {
+        int size = array.size();
+        int max = *max_element(array.begin(),array.end());
+        vector<int> output(size);
+        vector<int> count(max + 1, 0);
+        for (int i = 0; i < size; i++)
+            count[array[i]]++;
+
+        for (int i = 1; i <= max; i++)
+            count[i] += count[i - 1];
+
+        for (int i = size - 1; i >= 0; i--)
+        {
+            output[count[array[i]] - 1] = array[i];
+            count[array[i]]--;
+        }
+        return output;
+    }
+
 public:
     int heightChecker(vector<int> &heights)
     {
-        vector<int> sorted = heights;
-        sort(sorted.begin(),sorted.end());
+        vector<int> sorted = countSort(heights);
         int count = 0;
         for(int n=0 ; n<heights.size() ; n++)
             if(heights[n] != sorted[n])
