e5acfd4374173e41e07001069b55caa10367b7d5
Safiullah-Sohail <106798707+Safiullah-Sohail@users.noreply.github.com>
2022-11-21 15:11:45 +0500

    Time: 83 ms (68.23%), Space: 25.6 MB (28.45%) - LeetHub

---
+++ 1030-matrix-cells-in-distance-order/1030-matrix-cells-in-distance-order.cpp
@@ -0,0 +1,58 @@
+class Solution
+{
+    vector<vector<int>> countSort(vector<vector<int>> array)
+    {
+        int size = array.size();
+        int max = array[0][2];
+        for(int n=0 ; n<array.size() ; n++)
+            if(array[n][2] > max)
+                max = array[n][2];
+        vector<vector<int>> output(size);
+        vector<int> count(max + 1, 0);
+        for (int i = 0; i < size; i++)
+            count[array[i][2]]++;
+
+        for (int i = 1; i <= max; i++)
+            count[i] += count[i - 1];
+
+        for (int i = size - 1; i >= 0; i--)
+        {
+            output[count[array[i][2]] - 1] = array[i];
+            count[array[i][2]]--;
+        }
+        return output;
+    }
+    void selectionSort(vector<vector<int>> &array)
+    {
+        for (int i = 0; i < array.size(); i++)
+        {
+            int minIdx = array[i][2];
+            for (int j = i + 1; j < array.size(); j++)
+            {
+                int temp = array[j][2];
+                if (array[j][2] < minIdx)
+                    minIdx = array[j][2];
+            }
+            swap(array[minIdx], array[i]);
+        }
+    }
+public:
+    vector<vector<int>> allCellsDistOrder(int rows, int cols, int rCenter, int cCenter)
+    {
+        vector<vector<int>> ans;
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                int diff = abs(rCenter - i) + abs(cCenter - j);
+                ans.push_back({i, j, diff});
+            }
+        }
+        ans = countSort(ans);
+        for (vector<int> &curr : ans)
+        {
+            curr.pop_back();
+        }
+        return ans;
+    }
+};
